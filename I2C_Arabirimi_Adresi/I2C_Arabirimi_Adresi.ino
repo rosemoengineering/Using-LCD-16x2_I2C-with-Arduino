0e5d9926b94e2cd3c90a9c06bb38f33a854902d3
rosemoengineering <35057450+rosemoengineering@users.noreply.github.com>
2018-02-16 23:02:54 +0200

    Add files via upload

---
+++ I2C_Arabirimi_Adresi/I2C_Arabirimi_Adresi.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+
+boolean DeviceFound = false;
+byte error, address;
+
+void setup()
+{
+    Wire.begin();
+    Serial.begin(9600);
+    Serial.println("\nI2C Araniyor...");
+  
+    for(address = 0; address <= 127; address++ )
+    {
+       Wire.beginTransmission(address);
+       error = Wire.endTransmission();
+
+       if (error == 0)
+       {
+         Serial.print("I2C cihaz bulundu. Adresi: 0x");
+         if (address<16)   Serial.print("0");
+         Serial.println(address,HEX);
+  
+         DeviceFound = true;
+         break;
+       }
+       else if (error==4)
+       {
+         Serial.print("Bilinmeyen hata olustu. Adres: 0x");
+         if (address<16)  Serial.print("0");
+         Serial.println(address,HEX);
+       }
+    }
+    if (DeviceFound == false)
+       Serial.println("Herhangi bir bagli I2C bulunamadi\n");
+    else
+       Serial.println("Tamamlandi. Kolay gelsin.\n");          
+}
+ 
+void loop()
+{
+}
---
+++ hello_lcd/hello_lcd.ino
@@ -0,0 +1,24 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+
+// Set the LCD address 
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+
+
+void setup()
+{
+	
+  lcd.begin();// LCD ekranı balat
+  lcd.backlight();// Ekran arka ışığını aç
+  lcd.setCursor(0,0);
+  lcd.print("Hello, YOUTUBE!"); //İlk satıra yazalım
+  lcd.setCursor(1,1);
+  lcd.print("I am starting!"); //İkinci satıra yazalım
+}
+
+void loop()
+{
+
+}
